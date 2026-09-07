4da880c95931b831c660c770a78a0059e142486f
telenexus-admin <admin@telenexustechnologies.com>
2026-09-08 02:12:56 +0300

    Activate premium PropOS visual system

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './modules.css';
 import './extras.css';
 import './command.css';
+import './premium.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
