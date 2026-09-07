281be785b8bf3dd017c116ac0264d87bb1f9cf96
telenexus-admin <admin@telenexustechnologies.com>
2026-09-08 02:16:07 +0300

    Load premium screen-specific UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './modules.css';
 import './extras.css';
 import './command.css';
 import './premium.css';
+import './screen-premium.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
