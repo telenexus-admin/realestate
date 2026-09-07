5b93cc7f4bf9d416f08b21923f9bd977d1b0a543
telenexus-admin <admin@telenexustechnologies.com>
2026-09-08 02:43:21 +0300

    Load stateful interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './deepadmin.css';
 import './enterprise.css';
 import './enterprise2.css';
 import './interactions.css';
+import './interactions2.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
