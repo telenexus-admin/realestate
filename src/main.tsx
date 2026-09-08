41b0110486847bee153d20c983aa653327ec14e9
telenexus-admin <admin@telenexustechnologies.com>
2026-09-08 03:05:56 +0300

    Gate PropOS with secure session shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AuthShell from './auth-shell';
 import './styles.css';
 import './modules.css';
 import './extras.css';
@@ -16,9 +16,10 @@ import './enterprise2.css';
 import './interactions.css';
 import './interactions2.css';
 import './workflow-execution.css';
+import './auth.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthShell />
   </React.StrictMode>
 );
